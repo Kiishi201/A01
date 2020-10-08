993d5353c1d7800b48819bc198d0b7d7fdbcecad
Kiishi201 <68674972+Kiishi201@users.noreply.github.com>
2020-10-08 11:30:23 -0400

    Add files via upload

--- ClientGui.java
+++ ClientGui.java
@@ -1,16 +1,30 @@
 import java.io.*;
 import java.net.*;
 import java.net.Socket;
-import java.io.;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.UnknownHostException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import Server.Request.Bibliography;
+
 
 import jdk.internal.org.jline.utils.InputStreamReader;
+import sun.awt.AWTAccessor.AccessibleContextAccessor;
 
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class ClientGui {
+public final class ClientGui {
     public static void main(String[] args) throws IOException{
         Socket s = null;
         PrintWriter p = null;
@@ -36,7 +50,7 @@ public class ClientGui {
 
         while ((fromServer = in.readline()) != null) {
             System.out.println("Server: " + fromServer );
-            if (fromServer.equals("close"))
+            if (!fromServer.equals("close"))
                 break;
 
             str = stdIn.readLine();
@@ -46,7 +60,110 @@ public class ClientGui {
 
 
         }
+        JFrame frame = new JFrame("SUBMIT");
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.ORANGE);
+        frame.setBounds(100, 100, 450, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        Font lblFont = newFont("Serif", Font.BOLD, 20);
+        JLabel[] uscore = new JLabel[15];
 
+        frame.setSize(300,300);
+         
+
+        Msg msg = new Msg();
+        titlewrd = new JLabel(" MESSAGE");
+        titlewrd.setBounds (260,100,500,150);
+        titlewrd.setFont(lblFont);
+
+        add (titlewrd);
+        //this.add(man);
+        man.setBounds(100,100,400,400);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(r);
+        frame.getContentPane().add(panel);
+
+
+
+        msglayout = new FlowLayout();
+        container = getContentPane();
+        setLayout(null);
+
+
+        sButton = new JButton("SUBMIT");
+        add(SButton);
+        sButton.setBounds(30,520, 50, 29);
+        sButton.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event){
+                    System.out.println("....."); // book description
+                }
+            }
+        );
+
+        uButton  = new JButton("UPDATE");
+        add(uButton);
+        uButton.setBounds(30,520, 50, 29);
+        uButton.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event){
+                    System.out.println("....."); // update book description
+                }
+            }
+        );
+
+        gButton  = new JButton("GET");
+        add(gButton);
+        gButton.setBounds(30,520, 50, 29);
+        gButton.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event){
+                    System.out.println("....."); // get book description
+                }
+            }
+        );
+
+        rButton  = new JButton("REMOVE");
+        add(rButton);
+        rButton.setBounds(30,520, 50, 29);
+        rButton.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event){
+                    System.out.println("....."); // remove book description
+                }
+            }
+        );
+
+
+
+
+
+        //panel.add(button);
+        //button.addActionListener(new Action());
+
+      /*  static class Action implements ActionListener{
+            public void actionPerformed (ActionEvent e) {
+                JFrame frame2 = new JFrame("click");
+                frame2.setVisible(true);
+                frame2.setSize(200,200);
+                JLabel label = new JLabel("   done  ");
+                JPanel panel = new JPanel();
+                frame2.add(panel);
+                panel.add(label);
+                
+
+            }
+        */
+
+
+            
+    
 
         }
         p.close();
