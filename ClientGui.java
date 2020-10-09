5ca20c4170527fe2f69d3fa3cd98ffcf5c1265ba
Kiishi201 <68674972+Kiishi201@users.noreply.github.com>
2020-10-09 01:55:23 -0400

    Add files via upload

--- ClientGui.java
+++ ClientGui.java
@@ -9,16 +9,11 @@ import java.io.PrintWriter;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
-import Server.Request.Bibliography;
-
-
-import jdk.internal.org.jline.utils.InputStreamReader;
-import sun.awt.AWTAccessor.AccessibleContextAccessor;
 
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -26,209 +21,197 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
 public class ClientGui extends JFrame implements ActionListener {
     private JTextField ipAddText;
-    private JTextArea  ipAddArea;
     private JTextField portNoText;
-    private JTextArea  portNoArea;
+    private JButton connectButton;
     private JButton sendButton;
     private JButton uButton;
     private JButton sButton;
     private JButton gButton;
     private JButton rButton;
     private final String conButton = "Connect";
-    private final String disconButton = "Disconnect"; 
+    private final String disconButton = "Disconnect";
     protected JTextArea serverMsg;
-    
-    private string textIP;
+    private JTextArea userMsg;
+
+    private String textIP;
     private int textPort;
     private JPanel panel;
+    private JPanel secondaryPanelOne;
+    private JPanel secondaryPanelTwo;
+    private JPanel buttonPanel;
 
+    private JLabel ipLabel;
+    private JLabel portLabel;
 
+    private Socket kkSocket = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
 
-
-
-    public ClientGui () {
-        this.panel = new JPanel();
-        this.ipAddText = new JTextField(10);
-        this.portNoText = new JTextField(5);
+    public ClientGui() {
+        this.panel = new JPanel(new GridLayout(3, 1));
+        this.secondaryPanelOne = new JPanel(new GridLayout(1, 5));
+        this.secondaryPanelTwo = new JPanel(new GridLayout(1, 2));
+        this.buttonPanel = new JPanel(new GridLayout(4, 1));
+        // this.ipAddText = new JTextField(10);
+        // this.portNoText = new JTextField(5);
 
         this.ipLabel = new JLabel("IP Address");
+        this.ipAddText = new JTextField(20);
         this.portLabel = new JLabel("Port");
-        this.serverMsg = new JTextArea(10, 30);
-        //this.serverMsg.setEditable(false);
+        this.portNoText = new JTextField(4);
+        this.connectButton = new JButton(conButton);
+        this.userMsg = new JTextArea();
+        // this.serverMsg = new JTextArea(10, 30);
+        // this.serverMsg.setEditable(false);
+        addActionListeners();
 
-        this.panel.add(ipLabel);
-        this.panel.add(portLabel);
-        this.panel.add(uButton);
-        this.panel.add(sButton);
-        this.panel.add(gButton);
-        this.panel.add(rButton);
-        this.panel.add(sendButton);
+        this.secondaryPanelOne.add(this.ipLabel);
+        this.secondaryPanelOne.add(this.ipAddText);
+        this.secondaryPanelOne.add(this.portLabel);
+        this.secondaryPanelOne.add(this.portNoText);
+        this.secondaryPanelOne.add(this.connectButton);
 
+        this.sButton = new JButton("SUBMIT");
+        this.uButton = new JButton("UPDATE");
+        this.gButton = new JButton("GET");
+        this.rButton = new JButton("REMOVE");
 
-    
+        this.buttonPanel.add(this.sButton);
+        this.buttonPanel.add(this.uButton);
+        this.buttonPanel.add(this.gButton);
+        this.buttonPanel.add(this.rButton);
 
-        public static void main(String[] args) throws IOException{
-            Socket s = null;
-            PrintWriter p = null;
-            BufferedReader b = null;
+        this.secondaryPanelTwo.add(this.buttonPanel);
+        this.secondaryPanelTwo.add(this.userMsg);
 
-            try {
-                s = new Socket();
-                System.out.println("Connected");
+        this.sendButton = new JButton("Send Message");
 
-                p =  new PrintWriter(s.getOutputStream(), true);
-                b = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        this.panel.add(this.secondaryPanelOne);
+        this.panel.add(this.secondaryPanelTwo);
+        this.panel.add(this.sendButton);
+        this.add(this.panel);
+        this.setSize(400, 300);
+        this.setVisible(true);
+    }
 
-            } catch (UnknownHostException e) {
-                System.err.println("x     ");
-                System.exit(1);
-            } catch (IOException e) {
-                System.err.println("Could not listen on port: 4444");
-                System.exit(1);
-            }
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            System fromServer;
-            String str;
+    void addActionListeners() {
+        this.connectButton.addActionListener(new ActionListener() {
 
-            while ((fromServer = in.readline()) != null) {
-                System.out.println("Server: " + fromServer );
-                if (!fromServer.equals("close"))
-                    break;
-
-                str = stdIn.readLine();
-            if (str != null) {
-                System.out.println("Client: " + str);
-                out.println(str);
-
-
-            }
-
-
-
-            JFrame frame = new JFrame("SUBMIT");
-            frame.setVisible(true);
-            frame.getContentPane().setBackground(Color.ORANGE);
-            frame.setBounds(100, 100, 450, 600);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().setLayout(null);
-            Font lblFont = newFont("Serif", Font.BOLD, 20);
-            JLabel[] uscore = new JLabel[15];
-
-            frame.setSize(300,300);
-            
-
-            Msg msg = new Msg();
-            titlewrd = new JLabel(" MESSAGE");
-            titlewrd.setBounds (260,100,500,150);
-            titlewrd.setFont(lblFont);
-
-            add (titlewrd);
-            //this.add(man);
-            man.setBounds(100,100,400,400);
-
-            JPanel panel = new JPanel();
-            panel.setBounds(r);
-            frame.getContentPane().add(panel);
-
-
-
-            msglayout = new FlowLayout();
-            container = getContentPane();
-            setLayout(null);
-
-
-            sButton = new JButton("SUBMIT");
-            add(SButton);
-            sButton.setBounds(30,520, 50, 29);
-            sButton.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent event){
-                        System.out.println("....."); // book description
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ip = ipAddText.getText();
+                int port = Integer.parseInt(portNoText.getText());
+                if (connectButton.getText() == conButton && connectToPort(ip, port)) {
+                    connectButton.setText(disconButton);
+                } else if (connectButton.getText() == disconButton) {
+                    try {
+                        out.close();  // change variables
+                        in.close();
+                        kkSocket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
                     }
+                    connectButton.setText(conButton);
                 }
-            );
-
-            uButton  = new JButton("UPDATE");
-            add(uButton);
-            uButton.setBounds(30,520, 50, 29);
-            uButton.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent event){
-                        System.out.println("....."); // update book description
-                    }
-                }
-            );
-
-            gButton  = new JButton("GET");
-            add(gButton);
-            gButton.setBounds(30,520, 50, 29);
-            gButton.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent event){
-                        System.out.println("....."); // get book description
-                    }
-                }
-            );
-
-            rButton  = new JButton("REMOVE");
-            add(rButton);
-            rButton.setBounds(30,520, 50, 29);
-            rButton.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent event){
-                        System.out.println("....."); // remove book description
-                    }
-                }
-            );
-
-
-
-
-
-            //panel.add(button);
-            //button.addActionListener(new Action());
-
-        /*  static class Action implements ActionListener{
-                public void actionPerformed (ActionEvent e) {
-                    JFrame frame2 = new JFrame("click");
-                    frame2.setVisible(true);
-                    frame2.setSize(200,200);
-                    JLabel label = new JLabel("   done  ");
-                    JPanel panel = new JPanel();
-                    frame2.add(panel);
-                    panel.add(label);
-                    
-
-                }
-            */
-
-
-                
-        
-
             }
-            p.close();
-            b.close();
-            stdIn.close();
-            s.close();
 
+        });
 
+        this.sendButton.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = userMsg.getText();
+                if(message!= "" && isConnected()) {
+                    out.write(message);
+                    out.flush();
+                }
             }
-            
+        });
+    }
+    /*
+     * public void actionPerformed(ActionEvent e) { if(e.getSource() == sendButton)
+     * { new postDialog();
+     * 
+     * } else if (e.getSource() == rButton) { if (this.rbutton.getText()==
+     * conButton) { try {
+     * 
+     * 
+     * } } } }
+     */
 
-            
+    // Socket s = null;
+    // PrintWriter p = null;
+    // BufferedReader b = null;
 
+    // try {
+    // s = new Socket();
+    // System.out.println("Connected");
 
+    // p = new PrintWriter(s.getOutputStream(), true);
+    // b = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+    // BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+    // String fromServer;
+    // String str;
+    // while ((fromServer = b.readLine()) != null) {
+    // System.out.println("Server: " + fromServer);
+    // if (!fromServer.equals("close"))
+    // break;
+
+    // str = stdIn.readLine();
+    // if (str != null) {
+    // System.out.println("Client: " + str);
+    // System.out.println(str);
+
+    // }
+    // }
+    // b.close();
+    // stdIn.close();
+    // s.close();
+    // } catch (UnknownHostException e) {
+    // System.err.println("x ");
+    // System.exit(1);
+    // } catch (IOException e) {
+    // System.err.println("Error");
+    // System.exit(1);
+    // }
+
+    public static void main(String args[]) {
+        try {
+            new ClientGui();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
-}   
- 
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
 
+    }
+
+    Boolean connectToPort(String ip, int port) {
+        try {
+            kkSocket = new Socket(ip, port);
+            out = new PrintWriter(kkSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+            return true;
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: taranis.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: taranis.");
+            System.exit(1);
+        }
+        return false;
+    }
+
+    Boolean isConnected() {
+        return !kkSocket.isClosed();
+    }
+}
